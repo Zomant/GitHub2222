2c65e236d203d3b4e63bff505540ed4f099933d3
Zomant <zomantp3@gmail.com>
2022-11-28 13:13:10 +0530

    28-11-2022 latest commit

--- app/src/main/java/com/example/github2222/MainActivity.java
+++ app/src/main/java/com/example/github2222/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(this, "Hi all t28-11-2022a vijay", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Hi all t28-11-2022a vijayee", Toast.LENGTH_SHORT).show();
 
 
         Toast.makeText(this, "production", Toast.LENGTH_SHORT).show();
